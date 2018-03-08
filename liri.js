ac5fc020e9c21fc9e7c4cb741bdc612ff1d86eb0
Regan Loyola <loyolare96@gmail.com>
2018-03-08 12:36:32 -0800

    Finished do-what-it-says

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require('./keys.js');
 var Twitter = require('twitter');
 var SpotifyNode = require('node-spotify-api');
 var request = require('request');
+const fs = require('fs');
 
 
 var spotify = new SpotifyNode(keys.spotify);
@@ -33,22 +34,37 @@ else if (process.argv[2] == "spotify-this-song") {
     });
 }
 else if (process.argv[2] == "movie-this") {
-    request("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-        // If the request is successful (i.e. if the response status code is 200)
+    request("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             console.log("Title: " + JSON.parse(body).Title);
             console.log("Released: " + JSON.parse(body).Released)
-      
-          // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-          console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings["1"].Value)
-          console.log("Country: " + JSON.parse(body).Country);
-          console.log("Language: " + JSON.parse(body).Language);
-          console.log("Plot: " + JSON.parse(body).Plot);
-          console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings["1"].Value)
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
         }
-      });
-      
+    });
+
+}
+else if (process.argv[2] == "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, response) {
+        if (error) {
+            return console.log(error);
+        }
+        var newData = response.split(",");
+
+        spotify.search({ type: 'track', query: newData[1], limit: 5 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log("Artist: " + data.tracks.items["0"].album.artists["0"].name)
+            console.log("Track name: " + data.tracks.items["0"].name)
+            console.log("Album: " + data.tracks.items["0"].album.name)
+            if (data.tracks.items["0"].preview_url != null) {
+                console.log("Preview URL: " + data.tracks.items["0"].preview_url);
+            }
+        });
+    });
 }
